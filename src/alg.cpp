25c7daad379b20c598f8c6519e790e730f55fa56
Peace0death <160275048+Peace0death@users.noreply.github.com>
2024-03-02 18:05:57 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,45 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+    if (value <= 1) {
+        return false;
+    }
+    for (uint64_t i = 2; i * i <= value; i++) {
+        if (value % i == 0) {
+            return false;
+        }
+    }
+    return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+    int count = 0;
+    int number = 2;
+    
+    while (count < n) {
+        if (checkPrime(number)) {
+                count++;
+            }
+        number++;
+    }
+    return number-1;
+    
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+    for (int i = value; i>= value; i++) {
+        if (checkPrime(i)) {
+            return i;
+        }
+    }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+    int b = 0;
+    for (int i = 0; i < hbound; i++) {
+        if (checkPrime(i)) {
+            b = b + i;
+        }
+    }
+    return b;
 }
